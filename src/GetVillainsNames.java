e3f9f122269491dc1d2645ab49831611e9ed8d9e
Vladimir Zorov <vl.zorov@gmail.com>
2022-10-23 19:58:18 +0300

    Add changes

--- src/GetVillainsNames.java
+++ src/GetVillainsNames.java
@@ -1,2 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class GetVillainsNames {
+
+    public static void main(String[] args) throws SQLException {
+
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "123456");
+
+        Connection connection =
+                DriverManager.getConnection("//localhost:3306/minions_db/");
+
+
+    }
 }
